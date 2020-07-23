2b872a15d530a8c78bfd5bac801dc5a5a37af823
StevanZivlak <zivlaks@yahoo.com>
2020-07-23 12:56:59 +0200

    Updates

---
+++ precache-manifest.cc10e1bd43eb966581b9f1046d8a5199.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a59cd367c9ffa6655acfe614de17e03",
+    "url": "/TypingGame/index.html"
+  },
+  {
+    "revision": "9d9024d982ad8ba0e70d",
+    "url": "/TypingGame/static/js/2.ddedabb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TypingGame/static/js/2.ddedabb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24209685fdf46b5bd42c",
+    "url": "/TypingGame/static/js/main.44c948a4.chunk.js"
+  },
+  {
+    "revision": "0d844ca431c0c2b54932",
+    "url": "/TypingGame/static/js/runtime-main.516b345a.js"
+  }
+]);
